e03b1410264e2660220c19f9bf64600136d7b6c8
Sailortr <165952108+Sailortr@users.noreply.github.com>
2024-10-03 23:33:44 +0000

    Deploying to gh-pages from @ Sailortr/goit-js-hw-12@07100aba884732c421eea786b27936e040e42050 🚀

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import{S as m,a as p,i}from"./assets/vendor-CRCB-GUD.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const u=document.querySelector(".search-input"),d=document.getElementById("gallery"),f=document.getElementById("myForm"),y=new m(".gallery li > a",{captionsData:"alt",captionDelay:250}),g=document.getElementById("load-more");let l=1,c="";async function h(){const s=u.value.trim();let o;s!==""&&s!==c?(l=1,c=s,d.innerHTML="",o=document.getElementById("loader-container")):o=document.getElementById("loader-more-container");const n=`https://pixabay.com/api/?key=46270894-c6cf89269c02f6d41aef898e4&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`;try{o.classList.remove("hidden");const t=(await p.get(n)).data;t.hits.length===0?i.error({title:"",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):(b(t.hits),g.classList.remove("hidden"),l++)}catch(e){i.error({title:"",message:`Sorry, ${e.message}! Please try again!`,position:"topRight"}),console.error("Hata:",e)}finally{o.classList.add("hidden")}}function b(s){const o=s.map(r=>`
+import{S as p,a as f,i as c}from"./assets/vendor-CRCB-GUD.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const m=document.querySelector(".search-input"),d=document.getElementById("gallery"),u=document.getElementById("search-form"),y=new p(".gallery li > a",{captionsData:"alt",captionDelay:250}),g=document.getElementById("load-more");let i=1,l="";async function h(){const s=m.value.trim();let r;s!==""&&s!==l?(i=1,l=s,d.innerHTML="",r=document.getElementById("loader-container")):r=document.getElementById("loader-more-container");const n=`https://pixabay.com/api/?key=46270894-c6cf89269c02f6d41aef898e4&q=${s}&image_type=photo&orientation=horizontal&safesearch=true&page=${i}&per_page=12`;try{r.classList.remove("hidden");const t=(await f.get(n)).data;t.hits.length===0?c.error({title:"",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):(b(t.hits),g.classList.remove("hidden"),i++)}catch(e){c.error({title:"",message:`Sorry, ${e.message}! Please try again!`,position:"topRight"}),console.error("Hata:",e)}finally{r.classList.add("hidden")}}function b(s){const r=s.map(o=>`
     <li class="card">
-  <a href="${r.largeImageURL}">
-    <img src="${r.webformatURL}" alt="${r.tags}" />
+  <a href="${o.largeImageURL}">
+    <img src="${o.webformatURL}" alt="${o.tags}" />
   </a>
   <div class="info">
-    <p class="info-text"><b>Likes</b> ${r.likes}</p>
-    <p class="info-text"><b>Views</b> ${r.views}</p>
-    <p class="info-text"><b>Comments</b> ${r.comments}</p>
-    <p class="info-text"><b>Downloads</b> ${r.downloads}</p>
+    <p class="info-text"><b>Likes</b> ${o.likes}</p>
+    <p class="info-text"><b>Views</b> ${o.views}</p>
+    <p class="info-text"><b>Comments</b> ${o.comments}</p>
+    <p class="info-text"><b>Downloads</b> ${o.downloads}</p>
   </div>
 </li>
     
-    `);d.innerHTML=o.join(""),y.refresh()}f.addEventListener("submit",async s=>{s.preventDefault(),await h()});
+    `);d.innerHTML+=r.join(""),y.refresh()}u.addEventListener("submit",async s=>{s.preventDefault(),await h()});
 //# sourceMappingURL=index.js.map
